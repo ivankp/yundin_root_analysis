77f91204db4333b9cab1ebcdace256ee3f96c22f
ivankp <ivan.pogrebnyak@gmail.com>
2014-12-29 14:41:17 -0500

    added .gitignore

--- run.sh
+++ run.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+#CPLUS_INCLUDE_PATH=/home/ivanp/local/LHAPDF5/include:$CPLUS_INCLUDE_PATH
+#LIBRARY_PATH=/home/ivanp/local/LHAPDF5/lib:$LIBRARY_PATH
+#LD_LIBRARY_PATH=/home/ivanp/local/LHAPDF5/lib:$LD_LIBRARY_PATH
+
+source /msu/opt/cern/rootSL6/v5.34.18_64/bin/thisroot.sh
+
+__LHADIR=/home/mondrag3/software/lhapdf
+PATH=$__LHADIR/bin:$PATH
+CPLUS_INCLUDE_PATH=$__LHADIR/include:
+LIBRARY_PATH=
+LD_LIBRARY_PATH=$__LHADIR/lib:$LD_LIBRARY_PATH
+LHAPATH=/home/mondrag3/software/lhapdf/share/lhapdf/PDFsets/
+
 JETS=3
-HAM=hammer.py     # full path to hammer.py
-ANA=ham-higgs.py  # full path to ham-higgs.py
+HAM=./hammer.py     # full path to hammer.py
+ANA=./ham-higgs.py  # full path to ham-higgs.py
 SEED=100_100
 FPDF=CT10nlo      # PDF set used in NTuples
 TPDF=CT10nlo      # PDF to use in analysis
 for RESC in mult_higgs fixed_higgs; do
   for PART in B V I RS; do
     for SCL in 1 0.5 2; do
-      FILE=H${JETS}.0j_GGF_${PART}_4000_pt25.0_eta4.5_r${SEED}.root
+      FILE=/msu/data/t3work4/luisonig/Ntuples/H${JETS}.0j_GGF_${PART}_4000_pt25.0_eta4.5_r${SEED}.root
       OUT="ATLAS-H$JETS-r$SEED-$TPDF-$RESC-$PART-$SCL"
       $HAM --noapplgrid --noloopsim --analysis $ANA -o "$OUT-%s" \
            -n $JETS -s $SCL -f $FPDF -t $TPDF --rescaler=$RESC $FILE > ${OUT}.log

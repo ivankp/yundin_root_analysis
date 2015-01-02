adbe2ee52885d0cdd2bec284f690d4178c04d6c7
ivankp <ivan.pogrebnyak@gmail.com>
2015-01-02 15:55:02 -0500

    loop in test_run

--- test_run.sh
+++ test_run.sh
@@ -6,6 +6,8 @@ CPLUS_INCLUDE_PATH=$__LHADIR/include:$CPLUS_INCLUDE_PATH
 LIBRARY_PATH=$__LHADIR/lib:$LIBRARY_PATH
 LD_LIBRARY_PATH=$__LHADIR/lib:$LD_LIBRARY_PATH
 
-./hammer.py --noapplgrid --noloopsim --analysis "./ham-higgs.py" -o "born-%s" \
-            -n 3 -s 1 -f CT10nlo -t CT10nlo --rescaler="fixed_higgs" \
-            /home/ivanp/work/bh_analysis/data/ten/born_bh.root
+for mu in fixed_higgs mult_higgs; do
+./hammer.py --noapplgrid --noloopsim --analysis "./ham-higgs.py" -o "born-$mu-%s" \
+            -n 3 -s 1 -f CT10nlo -t CT10nlo --rescaler=$mu \
+            /home/ivanp/work/bh_analysis/data/10k/born_bh.root
+done
